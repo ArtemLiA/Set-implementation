9434f78b0f87f292a79208eda57eb62a34029e8e
Artem Liakhov <Liakhov.Artem@yandex.ru>
2022-11-30 16:45:34 +0300

    Set: was added set class and implementation of all set's methods in Set.cpp

--- Set.cpp
+++ Set.cpp
@@ -12,24 +12,46 @@ Set<T>::Set(){
     root = nullptr;
 }
 
-template <class T>
+template<class T>
 Set<T>::Set(T def){
-    root = new BinaryTreeElem<T>;
-    root->value = def;
-    root->right = nullptr;
-    root->left = nullptr;
+    BTree_insert(root, def);
 }
 
 template<class T>
 Set<T>::Set(std::initializer_list<T> list){
     for (auto val: list){
-        this->insert_elem(val);
+        BTree_insert(root, val);
     }
 }
 
 template<class T>
-void Set<T>::insert_elem(T val) {
-    insert_to_binary_tree(root, val);
+void Set<T>::insert(T value) {
+    BTree_insert(root, value);
+}
+
+template<class T>
+void Set<T>::remove(T value) {
+    BTree_remove(root, value);
+}
+
+template<class T>
+uint Set<T>::size() {
+    BTree_size(root);
+}
+
+template<class T>
+bool Set<T>::is_in_set(T value) {
+    BTree_is_in_tree(root, value);
+}
+
+template<class T>
+bool Set<T>::is_empty() {
+    return (root == nullptr);
+}
+
+template<class T>
+Set<T>::~Set() {
+    BTree_clear(root);
 }
 
 
--- Set.h
+++ Set.h
@@ -6,56 +6,25 @@
 #ifndef SET_PROJECT_SET_H
 #define SET_PROJECT_SET_H
 #include <iostream>
+#include "BTree.cpp"
 
-typedef unsigned int uint;
-
-template<class T>
-struct BinaryTreeElem{
-    T value;
-    BinaryTreeElem* right;
-    BinaryTreeElem* left;
-};
-
-template<class T>
-void insert_to_binary_tree(BinaryTreeElem<T>*& root, T val){
-    if (root == nullptr){
-        root = new BinaryTreeElem<T>;
-        root->right = nullptr;
-        root->left = nullptr;
-        root->value = val;
-        return;
-    }
-    if (root->value == val){
-        return;
-    }
-    if (root->value < val){
-        insert_to_binary_tree(root->right, val);
-        return;
-    }
-    if (root->value > val){
-        insert_to_binary_tree(root->left, val);
-        return;
-    }
-}
-
-
-
-
+//Implementing a set based on binary tree
 template<class T>
 class Set{
-protected:
-    BinaryTreeElem<T>* root;
+private:
+    BTree<T>* root;
 public:
     Set();
-    Set(T def);
-    Set(std::initializer_list<T>);
-    void insert_elem(T val);
-    void delete_elem(T val);
-    size_t size();
-    bool is_in_set(T val);
-    bool empty(T val);
-    void print();
+    explicit Set(T def);
+    Set(std::initializer_list<T> list);
+    void insert(T value);
+    void remove(T value);
+    uint size();
+    bool is_in_set(T value);
+    bool is_empty();
+    ~Set();
 };
+
 
 #include "Set.cpp"
 #endif //SET_PROJECT_SET_H
